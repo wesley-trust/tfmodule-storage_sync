c78de45a68524ddb0cb3a52c376fbc6194a43ad5
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-19 13:30:48 +0100

    Update network version

--- resource_network.tf
+++ resource_network.tf
@@ -1,11 +1,12 @@
 # Create spoke network dependencies
 module "service_network" {
   count                   = var.provision_private_link == true ? 1 : 0
-  source                  = "github.com/wesley-trust/tfsubmodule-network?ref=v1-network"
+  source                  = "github.com/wesley-trust/tfsubmodule-network?ref=v1.1-network"
   service_name            = "${var.service_name}-Private"
   service_location_prefix = local.service_location_prefix
   resource_location       = module.resource_group.location
   resource_group_name     = module.resource_group.name
   resource_environment    = var.service_environment
+  resource_deployment     = var.service_deployment
   resource_address_space  = var.resource_address_space
 }
